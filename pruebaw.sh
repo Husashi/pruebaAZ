0513a0dacce0b2fa954c167c43ba300578498a77
Husashi <eduarcross@gmail.com>
2022-11-15 00:57:41 -0300

    Create pruebaw.sh

---
+++ pruebaw.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+# Update apt cache.
+sudo apt-get update
+
+# Set the home page.
+echo "<html><body><h2>Hola weycita, te amo mucho</h2></body></html>" | sudo tee -a /var/www/html/index.html
